00c0c00ff2188bf0aa34000ea69151fd4450f2d4
gomaa66 <122218564+gomaa66@users.noreply.github.com>
2023-01-24 02:04:42 +0200

    Add files via upload

---
+++ arrow.py
@@ -0,0 +1,12 @@
+for i in range(0,5,1):
+	for j in range(i+1):
+		print("    * ", end="")
+	print("\n")
+	
+print("* * * * * * * * * * * * * * * * * ")
+
+
+for i in range(6, 0, -1):    
+	for j in range(0, i - 1,1):  
+		print("    *", end=' ')  
+	print("\n")
